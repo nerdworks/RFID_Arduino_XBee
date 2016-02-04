9c1d82524e82431d2c505f4a5b40b74d8e7b0fb0
Jøran Kvalvaag <jk@nerdworks.org>
2016-02-04 15:27:52 +0100

    Installed elapsedMillis

--- RFID_Arduino_XBee.ino
+++ RFID_Arduino_XBee.ino
@@ -4,6 +4,18 @@
  Author:	joran.kvalvaag
 */
 
+
+#include <elapsedMillis.h>
+// see warning above about FioV3
+int led = 13;
+// Pin 13 has an LED connected on most Arduino boards.
+// if using Arduino IDE 1.5 or above you can use pre-defined
+// LED_BUILTIN  instead of 'led'
+// 
+elapsedMillis timer0;
+#define interval 1000
+// the interval in mS 
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 
