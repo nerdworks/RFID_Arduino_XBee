3a451dfe89fd5684970f0e5de3de2309ee7d41a1
Jøran Kvalvaag <jk@nerdworks.org>
2016-02-04 15:20:14 +0100

    First Commit

---
+++ RFID_Arduino_XBee.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		RFID_Arduino_XBee.ino
+ Created:	2/4/2016 3:19:12 PM
+ Author:	joran.kvalvaag
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  //Kom igjen....
+}
