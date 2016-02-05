26c8c784969a2149f32000074d3f755f01cdf938
Jøran Kvalvaag <jk@nerdworks.org>
2016-02-05 12:37:06 +0100

    Playing with state machine

--- RFID_Arduino_XBee.ino
+++ RFID_Arduino_XBee.ino
@@ -4,26 +4,37 @@
  Author:	joran.kvalvaag
 */
 
+// Se på state maskin i eksempelbruk av elapsedMillis....
+// og her: https://hackingmajenkoblog.wordpress.com/2016/02/01/the-finite-state-machine/
 
+
+// Read about elapsedMillis here:
+// http://www.forward.com.au/pfod/ArduinoProgramming/TimingDelaysInArduino.html
 #include <elapsedMillis.h>
-// see warning above about FioV3
-int led = 13;
-// Pin 13 has an LED connected on most Arduino boards.
-// if using Arduino IDE 1.5 or above you can use pre-defined
-// LED_BUILTIN  instead of 'led'
-// 
-elapsedMillis timer0;
-#define interval 1000
-// the interval in mS 
 
-// the setup function runs once when you press reset or power the board
+// just for testing
+int led = 13; // Pin 13 has an LED connected on most Arduino boards.
+
+// Define timers
+elapsedMillis timer0;
+#define timer0interval 1000 // the interval in mS 
+
+
 void setup() {
 
+	
+	pinMode(led, OUTPUT); // initialize the digital pin as an output.
+	timer0 = 0; // clear the timer at the end of startup
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  //Kom igjen....
-	// JK
-	// 789
+  
+
+	if (timer0 > timer0interval) {
+		timer0 -= timer0interval; // reset the timer
+		int ledPin = digitalRead(led); // read the current state and write the opposite
+		digitalWrite(led, !ledPin); // switch the LED
+	}
+
 }
